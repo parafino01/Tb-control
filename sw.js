57123c8d4083685181dac07f42c80dd500635e05
parafino01 <113385659+parafino01@users.noreply.github.com>
2026-02-12 22:35:26 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'tb-control-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+// Instala e guarda os arquivos no cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Responde mesmo sem internet
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
